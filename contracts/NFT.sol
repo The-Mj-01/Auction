7418b4ffc54e385c69898d16ae09fa872e38e65c
The Mj <the.king.mj99@gmail.com>
2023-03-10 00:16:11 +0330

    Create simple NFT ERC721 smart contract!

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,7 +1,18 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract NFT {
-  constructor() public {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+  uint256 private _tokenIdCounter;
+
+  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+    _tokenIdCounter = 0;
+  }
+
+  function mint(address _to, uint256 _tokenId) external onlyOwner returns (uint256) {
+
+    _safeMint(_to, _tokenId);
+    return _tokenIdCounter;
   }
 }
